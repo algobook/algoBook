054ef7a75b3b7fc54e46caf6daced6e7517c91c4
Piyush Agrawal <poush12@gmail.com>
2016-10-31 19:07:45 +0530

    settings update

--- algoBook/settings.py
+++ algoBook/settings.py
@@ -130,8 +130,8 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
