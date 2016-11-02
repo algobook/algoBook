8a3a112ad209a9595bb4f17c18435d4fa82b39a8
Piyush Agrawal <poush12@gmail.com>
2016-11-02 18:39:44 +0530

    viewport fix

--- main/models.py
+++ main/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Algo(models.Model):
     name = models.CharField(max_length=256)
+    description = models.CharField(max_length=500)
+    lang = models.CharField(max_length=10)
     created_at = models.DateTimeField('created_at')
     updated_at = models.DateTimeField('updated_at')
     code = models.CharField(max_length=10000)
