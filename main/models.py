4e550878487fdb1204e5cdc1343d410aeeed5241
Piyush Agrawal <poush@users.noreply.github.com>
2016-11-28 21:07:12 +0530

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import date
+from django.core.urlresolvers import reverse
+from django.forms import ModelForm
 
 # Create your models here.
 class Algo(models.Model):
@@ -9,11 +12,10 @@ class Algo(models.Model):
     updated_at = models.DateTimeField('updated_at')
     code = models.CharField(max_length=10000)
     slug = models.CharField(max_length=256)
+#TODO:
+# Add fields for upvotes
 
 
-from datetime import date
-from django.core.urlresolvers import reverse
-from django.forms import ModelForm
 
 # User will get badges
 class Badges(models.Model):
@@ -45,35 +47,6 @@ class User(models.Model):
 	password = models.CharField(max_length = 30, null = False, blank = False)
     # badges not integrated yet
 	#badges = models.ForeignKey(Badges, on_delete = models.DO_NOTHING)
-
-	def __unicode__(self):
-		return self.name
-
-	def __str__(self):
-		return self.name
-
-# Question asked by user, may be here you are representing it by Algo
-class Question(models.Model):
-	asked_by = models.ForeignKey(User, on_delete = models.DO_NOTHING)
-	title = models.CharField(max_length = 30, primary_key = True)
-	description = models.TextField(max_length = 1000, null = False, blank = False)
-	votes = models.IntegerField()
-	tags = models.ManyToManyField(Tags)
-	timestamp = models.DateTimeField(auto_now = False, auto_now_add = True)
-
-	def __unicode__(self):
-		return self.name
-
-	def __str__(self):
-		return self.name
-
-# Answer given by user
-class Answer(models.Model):
-	answered_by = models.ForeignKey(User, on_delete = models.DO_NOTHING)
-	question_is = models.ForeignKey(Question, on_delete = models.DO_NOTHING)
-	description = models.TextField(max_length = 1000, null = False, blank = False)
-	timestamp = models.DateTimeField(auto_now = False, auto_now_add = True)
-	accepted = models.BooleanField(default = False)
 
 	def __unicode__(self):
 		return self.name
