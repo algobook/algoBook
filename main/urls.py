da75760f70e24a56d81e18aec16bd22768c95589
Piyush Agrawal <poush12@gmail.com>
2016-11-30 14:57:11 +0530

    auth views and login form added

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+
     url(r'^$', views.index, name='index'),
     # url(r'^search$',views.search, name='index'),
     url(r'^(?P<query>[\w+-]+)/$', views.search, name="search_algo")
