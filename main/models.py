db8acc7fa695ba748475537e7f21caede361704a
jainaman224 <jainaman224@gmail.com>
2016-11-30 14:10:09 +0530

    update

--- main/models.py
+++ main/models.py
@@ -2,17 +2,24 @@ from django.db import models
 from datetime import date
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Algo(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.CharField(max_length=500)
-    lang = models.CharField(max_length=10)
-    created_at = models.DateTimeField('created_at')
-    updated_at = models.DateTimeField('updated_at')
-    code = models.CharField(max_length=10000)
-    slug = models.CharField(max_length=256)
+	name = models.CharField(max_length = 256)
+	description = models.CharField(max_length = 500)
+	lang = models.CharField(max_length = 10)
+	created_at = models.DateTimeField('created_at', auto_now = True)
+	updated_at = models.DateTimeField('updated_at', auto_now = True)
+	code = models.CharField(max_length = 10000)
+	slug = models.CharField(max_length = 256)
+	tags = models.ManyToManyField('Tags')
+
+	def __unicode__(self):
+		return self.name
+
+	def __str__(self):
+		return self.name
 #TODO:
 # Add fields for upvotes
 
@@ -41,5 +48,11 @@ class Tags(models.Model):
 		return self.name
 
 
-class MyUser(AbstractBaseUser):
+class MyUser(User):
 	badges = models.ForeignKey(Badges, on_delete = models.DO_NOTHING)
+
+	def __unicode__(self):
+		return self.username
+	
+	def __str__(self):
+		return self.username
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
+	url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
     # url(r'^search$',views.search, name='index'),
     url(r'^(?P<query>[\w+-]+)/$', views.search, name="search_algo")
