f3db910636deefc98b9aca720ed3fa9484deea10
jainaman224 <jainaman224@gmail.com>
2016-11-30 15:14:03 +0530

    update

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-	url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
     # url(r'^search$',views.search, name='index'),
     url(r'^(?P<query>[\w+-]+)/$', views.search, name="search_algo")
