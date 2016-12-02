c788b2b33531d9f8f7eebd4da4fe25e691996303
Aman Jain <jainaman224@gmail.com>
2016-12-02 23:44:35 +0530

    trying to resolve heroku issues (#14)
    trying to resolve heroku issues

--- main/regbackend.py
+++ main/regbackend.py
@@ -1,4 +1,5 @@
 from registration.backends.default.views import RegistrationView
+import datetime
 
 from .models import Profile
 
@@ -7,5 +8,8 @@ class MyRegistrationView(RegistrationView):
         new_user = super(MyRegistrationView, self).register(request)
         user_profile = Profile()
         user_profile.user = new_user
+        user_profile.bio = ""
+        user_profile.location = ""
+        user_profile.birth_date = datetime.datetime(1947, 8, 15)
         user_profile.save()
         return user_profile
